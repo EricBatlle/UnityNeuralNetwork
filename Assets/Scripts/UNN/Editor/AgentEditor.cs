b0f8d4f9d7d67df74e231c57b6dc6b5be7b9804d
Eric <erikbc_95@hotmail.com>
2020-01-04 03:54:41 +0100

    added Editor functionalities to agents

---
+++ Assets/Scripts/UNN/Editor/AgentEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Base class used to facilitate sharing Editor functionalities through all Agent Editor derivates
+/// </summary>
+[CustomEditor(typeof(Agent))]
+public class AgentEditor : Editor
+{
+    Agent agent = null;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        agent = (Agent)target;
+        GUILayout.Space(10);
+        if (agent.initilized)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Save Neural Network"))
+                agent.SaveNeuralNetwork();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+        }        
+    }
+
+}
---
+++ Assets/Scripts/UNN/Editor/SimpleAgentEditor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// Even if it's empty, this class is needed to use AgentEditor functionalities
+/// </summary>
+[CustomEditor(typeof(SimpleAgent))]
+public class SimpleAgentEditor : AgentEditor
+{        
+}
