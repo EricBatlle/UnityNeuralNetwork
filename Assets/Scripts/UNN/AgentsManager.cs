b5490b9a6c7fd8b8a49d6c6f75039957d77fb787
Eric <erikbc_95@hotmail.com>
2020-01-05 21:34:35 +0100

    Manager can be controlled to start, stop and reset training cycles. It also can create his agents from a predefined NN, and create non-manager-dependant agents

--- Assets/Scripts/UNN/AgentsManager.cs
+++ Assets/Scripts/UNN/AgentsManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using static NeuralNetworkSerializer;
 
 /// <summary>
 /// Manager to control agents generations and training
 /// </summary>
 public abstract class AgentsManager : MonoBehaviour
 {
+    [Header("Manager")]
+    public bool trainOnStart = true;   //flag to start the train when the scene plays without button    
     [SerializeField] protected GameObject agentPrefab;
     [Space()]
     [SerializeField][Range(0f, 20f)]
@@ -19,28 +22,18 @@ public abstract class AgentsManager : MonoBehaviour
     [Space()]
     [Tooltip("Is recommended to have an even number of agents")]
     [SerializeField] protected int populationSize = 50;             //...to speed training cause training use the half of the population(population/2)
-    [SerializeField][DisplayWithoutEdit] protected int currentGenerationNumber = 0;
-    [SerializeField] protected int[] layers = new int[] { 1, 10, 10, 1 }; //1 input and 1 output
-    [Space()]
-    [SerializeField] protected List<NeuralNetwork> agentsNets;
-    [SerializeField] protected List<Agent> agentsList = null;        
-    protected GameObject agentsParentGO; //Used as agents parent on Unity's hierarchy
+    [DisplayWithoutEdit] protected int currentGenerationNumber = 0;
+    [DisplayWithoutEdit] protected int[] layers = new int[] { 1, 10, 10, 1 }; //1 input and 1 output
+    [Header("Neural Network to Load")]
+    public TextAsset neuralNetworkToLoad = null;
+    public bool createNetxGenerationWithThisNet = false;
 
-    /// <summary>
-    /// Start the training process every <c>newGenerationRateTime</c> and creates agent's parent Game Object
-    /// </summary>
-    private void Start()
-    {
-        CreateAgentsParentGO();
-        this.InvokeRepeating(TrainAgents, newGenerationRateTime);
-    }
-
-    [ContextMenu("Restart Training")]
-    private void RestartTraining()
-    {
-        CancelInvoke();
-        this.Start();
-    }
+    protected List<NeuralNetwork> agentsNets = null;
+    protected List<Agent> agentsList = null;        
+    protected GameObject agentsParentGO;                //Used as agents parent on Unity's hierarchy
+    [HideInInspector] public bool isTraining = false;   //True when the training process is on
+    [HideInInspector] public bool isTrainingStoped = false;   //True when the training process is on
+    private Coroutine trainingCoroutine = null;
 
     /// <summary>
     /// This is needed cause properties aren't called by UnityEditor changes
@@ -50,6 +43,54 @@ public abstract class AgentsManager : MonoBehaviour
         NewTimeScale = NewTimeScale;
     }
 
+    /// <summary>
+    /// Start training when loading the scene
+    /// </summary>
+    private void Start()
+    {
+        if (trainOnStart)
+            StartTraining();
+    }    
+
+    /// <summary>
+    /// Start the training process every <c>newGenerationRateTime</c> and creates agent's parent Game Object
+    /// </summary>
+    public void StartTraining()
+    {
+        isTraining = true;
+        isTrainingStoped = false;
+
+        CreateAgentsParentGO();
+        trainingCoroutine = this.InvokeRepeatingAndGetCoroutine(TrainAgents, newGenerationRateTime);
+    }
+
+    /// <summary>
+    /// Stop training process
+    /// </summary>
+    public void StopTraining()
+    {
+        isTraining = false;
+        isTrainingStoped = true;
+        StopCoroutine(trainingCoroutine);
+    }
+
+    /// <summary>
+    /// Reset training process from 0
+    /// </summary>
+    public void ResetTraining()
+    {
+        this.StopTraining();
+
+        //Clear variables
+        currentGenerationNumber = 0;
+        agentsNets = new List<NeuralNetwork>();
+        agentsList = new List<Agent>();
+        if (agentsParentGO != null)
+            DestroyImmediate(agentsParentGO);
+
+        this.StartTraining();
+    }    
+   
     /// <summary>
     /// Create agents GameObjects and initialize agents neural networks.
     /// Mutate the worst half and reset fitness values.
@@ -87,7 +128,7 @@ public abstract class AgentsManager : MonoBehaviour
         currentGenerationNumber++;        
         CreateAllAgentsGO();        
     }
-    
+
     /// <summary>
     /// Destroy previous game object agents and create new ones to add to agentsList
     /// </summary>
@@ -98,8 +139,29 @@ public abstract class AgentsManager : MonoBehaviour
 
         //Create new ones
         agentsList = new List<Agent>();
-        for (int i = 0; i < populationSize; i++)        
-            agentsList.Add(CreateAgentGO(i));                    
+
+        //Check if the creation of agents starts from scratch or loading a pre-existant neural network
+        bool createAgentsFromExternalNeuralNetwork = (neuralNetworkToLoad != null && createNetxGenerationWithThisNet) ? true : false;        
+        if (neuralNetworkToLoad == null && createNetxGenerationWithThisNet)
+            Debug.Log("Set Neural Network to load");
+        
+        //Create the agents Game Objects
+        if (createAgentsFromExternalNeuralNetwork)
+        {
+            Debug.Log("Created loaded generation");
+            for (int i = 0; i < populationSize; i++)
+            {
+                agentsList.Add(CreateAgentGO(i, neuralNetworkToLoad));
+            }
+            createNetxGenerationWithThisNet = false;
+        }
+        else
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                agentsList.Add(CreateAgentGO(i));
+            }
+        }    
     }
 
     /// <summary>
@@ -114,6 +176,40 @@ public abstract class AgentsManager : MonoBehaviour
         agent.transform.SetParent(agentsParentGO.transform);
         agent.Init(agentsNets[agentNumber]);
         return agent;
+    }
+
+    /// <summary>
+    /// Create agent Game Object and set his parent.
+    /// Also set his neural network and the inputs it will use.
+    /// The neural network is initially setted from an external neural network file.
+    /// </summary>
+    /// <param name="agentNumber"></param>
+    /// <param name="neuralNetworkToLoad"></param>
+    /// <returns></returns>
+    protected virtual Agent CreateAgentGO(int agentNumber, TextAsset neuralNetworkToLoad)
+    {
+        Agent agent = Instantiate(agentPrefab).GetComponent<Agent>();
+        agent.transform.SetParent(agentsParentGO.transform);
+        agent.Init(NeuralNetwork.LoadFromTextAsset(neuralNetworkToLoad));
+        return agent;
+    }
+
+    /// <summary>
+    /// This method allows the manager to create an Agent which is excluded of the manager training cycle.
+    /// It can be created with an external Neural Network or pick the smartest of the current cycle.
+    /// </summary>
+    /// <param name="neuralNetworkToLoad">Serialized neural network to load into the new agent</param>
+    public virtual void CreateSeparatedAgentGO()
+    {
+        if (neuralNetworkToLoad == null)
+            print("There is no Neural Network TextAsset attached");
+        else
+        {
+            Agent agent = Instantiate(agentPrefab).GetComponent<Agent>();
+            string jsonString = JsonManager.ReadJSONFile(neuralNetworkToLoad);
+            SerializableNeuralNetwork sNet = JsonManager.DeserializeFromJson<SerializableNeuralNetwork>(jsonString);
+            agent.Init(sNet.Deserialized());
+        }
     }
 
     /// <summary>
@@ -152,5 +248,5 @@ public abstract class AgentsManager : MonoBehaviour
     {
         agentsParentGO = new GameObject();
         agentsParentGO.name = "Agents";
-    }
+    }    
 }
---
+++ Assets/Scripts/UNN/Editor/AgentsManagerEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Base class used to facilitate sharing Editor functionalities through all Agent Editor derivates
+/// </summary>
+[CustomEditor(typeof(AgentsManager))]
+public class AgentsManagerEditor : Editor
+{
+    AgentsManager manager = null;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        manager = (AgentsManager)target;
+        GUILayout.Space(10);
+
+        if(EditorApplication.isPlaying)
+        {
+            if(manager.isTraining)
+            {
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Reset Training"))
+                    manager.ResetTraining();
+                if (GUILayout.Button("Stop Training"))
+                    manager.StopTraining();
+                GUILayout.EndHorizontal();
+            }
+            else if(manager.isTrainingStoped)
+            {
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Reset Training"))
+                        manager.ResetTraining();
+                if (GUILayout.Button("ReStart Train"))
+                    manager.StartTraining();
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                if (GUILayout.Button("Start Train"))
+                    manager.StartTraining();
+            }                
+
+            if (manager.neuralNetworkToLoad != null)
+            {
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Create Loaded Agent"))
+                    manager.CreateSeparatedAgentGO();
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.Space(10);
+    }
+
+}
