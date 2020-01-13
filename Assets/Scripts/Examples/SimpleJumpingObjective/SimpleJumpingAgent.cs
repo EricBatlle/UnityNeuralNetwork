87dc7d788d82275392eea3f970acecbaa1d7bddf
Eric <erikbc_95@hotmail.com>
2020-01-13 23:20:52 +0100

    NEW SCENARIO: SimpleJumpingObjective, Simple scenario but now agents needs to jump to achieve it's goal! First scenario with more than one output!

---
+++ Assets/Scripts/Examples/SimpleJumpingObjective/SimpleJumpingAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleJumpingAgent : SimpleAgent
+{
+    //Set velocity direction on X depending of the output
+    protected override void AgentAction()
+    {
+        ChangeAgentColor();
+        rBody.velocity = 2.5f * outputs[0] * new Vector3(1, 0, 0);
+        rBody.AddForce(new Vector3(0, outputs[1], 0), ForceMode.Impulse);
+    }
+}
