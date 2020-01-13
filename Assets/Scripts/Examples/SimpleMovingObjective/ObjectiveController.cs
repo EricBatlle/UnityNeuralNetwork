f8a0cf0b96ca71ab80b19638ad3895ebd521799b
Eric <erikbc_95@hotmail.com>
2020-01-13 23:19:47 +0100

    NEW SCENARIO: SimpleMovingObjective, Simple scenario but now the objectives changes its position randomly

---
+++ Assets/Scripts/Examples/SimpleMovingObjective/ObjectiveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveController : MonoBehaviour
+{
+    [SerializeField] private float changeLocationRate = 4f;
+    [SerializeField] private List<GameObject> spawnLocations = new List<GameObject>();
+
+    private void Start()
+    {
+        this.InvokeRepeating(ChangeObjectiveLocation, changeLocationRate);
+    }
+
+    private void ChangeObjectiveLocation()
+    {
+        //Get one random spawn location from the list
+        this.transform.localPosition = spawnLocations[Random.Range(0, spawnLocations.Count)].transform.localPosition;
+    }
+}
